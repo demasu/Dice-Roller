176c7af41aad95bda64a690e9ab62f193e183c0c
Joshua Brandt <joshua.brandt@cpanel.net>
2021-01-13 12:47:05 -0600

    Got interactive mode working, along with some improvements

--- dice_roller.cpp
+++ dice_roller.cpp
@@ -8,9 +8,12 @@
 
 // Function declarations
 
-void print_help_text(char *argv[]);
-void parse_options(int argc, char *argv[], int &num_dice, int &die_type, int &modifier);
-void roll_dice(int num_dice, int die_type, int modifier);
+int  main            ( int argc, char *argv[]                                                               );
+void parse_options   ( std::vector<std::string> user_arguments, int &num_dice, int &die_type, int &modifier );
+void interactive     ( int argc, char *argv[]                                                               );
+int  handle_input    ( std::string &input, bool &run                                                        );
+void roll_dice       ( int num_dice, int die_type, int modifier                                             );
+void print_help_text ( char *argv[])                                                                        ;
 
 // Enum for error codes
 enum Errors {
@@ -39,33 +42,36 @@ int main (int argc, char *argv[]) {
     if ( argc == 1 ) {
         // No arguments given
         // start interactive mode
-        // TODO: create interactive mode
-        print_help_text(argv);
-        return 1;
+        interactive(argc, argv);
+        return 0;
     }
     else {
         // Parse command line options
         try {
-            parse_options(argc, argv, num_dice, die_type, modifier);
+            std::vector<std::string> user_arguments;
+            for ( int i = 1; i < argc; i++ ) {
+                user_arguments.push_back(argv[i]);
+            }
+            parse_options(user_arguments, num_dice, die_type, modifier);
         }
-        catch ( int e ) {
+        catch ( Errors e ) {
             switch(e) {
-                case 0:
+                case INVALID_NUMBER_DIE:
                     std::cout << "[" << error_names[e] << "] " << "Invalid number of dice given" << std::endl;
                     break;
-                case 1:
+                case INVALID_DIE_TYPE:
                     std::cout << "[" << error_names[e] << "] " << "Invalid die type given" << std::endl;
                     break;
-                case 2:
+                case INVALID_MODIFIER:
                     std::cout << "[" << error_names[e] << "] " << "Invalid modifier given" << std::endl;
                     break;
-                case 3:
+                case UNABLE_CONVERT_NUM_DIE:
                     std::cout << "[" << error_names[e] << "] " << "Unable to convert the number of dice given into a number" << std::endl;
                     break;
-                case 4:
+                case UNABLE_CONVERT_DIE_TYPE:
                     std::cout << "[" << error_names[e] << "] " << "Unable to convert the type of die given into a number" << std::endl;
                     break;
-                case 5:
+                case UNABLE_CONVERT_MODIFIER:
                     std::cout << "[" << error_names[e] << "] " << "Unable to convert the modifier given into a number" << std::endl;
                     break;
             }
@@ -86,61 +92,56 @@ int main (int argc, char *argv[]) {
     return 0;
 }
 
-void parse_options (int argc, char *argv[], int &num_dice, int &die_type, int &modifier) {
-    std::vector<std::string> all_args(argv, argv + argc);
+void parse_options (std::vector<std::string> user_arguments, int &num_dice, int &die_type, int &modifier) {
     std::string num_dice_str;
     std::string die_type_str;
     std::string modifier_str;
 
-    for ( int i = 0; i < all_args.size(); i++ ) {
-        if ( all_args[i] == "--help" || all_args[i] == "--h" ) {
+    for ( int i = 0; i < user_arguments.size(); i++ ) {
+        std::string a = user_arguments[i];
+        if ( user_arguments[i] == "--help" || user_arguments[i] == "--h" ) {
             throw -1;
         }
-        if ( all_args[i] == argv[0] ) {
-            continue; // Skip the program name
-        }
-        else {
-            std::string a = all_args[i];
-            for ( int j = 0; j < a.length(); j++ ) {
-                if ( i < 2 ) {
-                    if ( isdigit(a[j]) && j < 1 ) {
-                        // First character should be a number
-                        num_dice_str = std::string(a.begin(), std::find(a.begin(), a.end(), 'd'));
-                        j = std::distance(a.begin(), std::find(a.begin(), a.end(), 'd'));
-                        continue;
-                    }
-                    else if ( j < 1 ) {
-                        Errors e = INVALID_NUMBER_DIE;
-                        throw e;
-                    }
+        for ( int j = 0; j < a.length(); j++ ) {
+            if ( i < 1 ) {
+                if ( isdigit(a[j]) && j < 1 ) {
+                    // First character should be a number
+                    num_dice_str = std::string(a.begin(), std::find(a.begin(), a.end(), 'd'));
+                    j = std::distance(a.begin(), std::find(a.begin(), a.end(), 'd'));
+                    continue;
+                }
+                else if ( j < 1 ) {
+                    Errors e = INVALID_NUMBER_DIE;
+                    throw e;
+                }
 
-                    if ( isdigit(a[j]) ) {
-                        std::string::iterator it = a.begin();
-                        std::advance(it, j);
-                        die_type_str = std::string(it, a.end());
-                        break;
-                    }
-                    else {
-                        Errors e = INVALID_DIE_TYPE;
-                        throw e;
-                    }
+                if ( isdigit(a[j]) ) {
+                    std::string::iterator it = a.begin();
+                    std::advance(it, j);
+                    die_type_str = std::string(it, a.end());
+                    break;
                 }
                 else {
-                    if ( a[j] == '+' ) {
-                        // Modifier is postive
-                        modifier_str = a[j];
-                    }
-                    else if ( a[j] == '-' ) {
-                        // Modifier is negative
-                        modifier_str = a[j];
-                    }
-                    else if ( isdigit(a[j]) ) {
-                        modifier_str += a[j];
-                    }
-                    else {
-                        Errors e = INVALID_MODIFIER;
-                        throw e;
-                    }
+                    Errors e = INVALID_DIE_TYPE;
+                    throw e;
+                }
+            }
+            else {
+                if ( a[j] == '+' ) {
+                    // Modifier is postive
+                    modifier_str = a[j];
+                }
+                else if ( a[j] == '-' ) {
+                    // Modifier is negative
+                    modifier_str = a[j];
+                }
+                else if ( isdigit(a[j]) ) {
+                    modifier_str += a[j];
+                }
+                else {
+                    std::cerr << "# There was an error parsing the modifier string: [" << a << "]" << std::endl;
+                    Errors e = INVALID_MODIFIER;
+                    throw e;
                 }
             }
         }
@@ -173,21 +174,94 @@ void parse_options (int argc, char *argv[], int &num_dice, int &die_type, int &m
     return;
 }
 
-void print_help_text (char *argv[]) {
+void interactive (int argc, char *argv[]) {
+    bool run = true;
+    std::cout << "Welcome to the dice roller!" << std::endl;
+    std::cout << "Follow the prompts to roll as many dice as you want." << std::endl;
+    std::cout << "To stop the program type in either 'q', 'quit', or press ctrl + d at any time." << std::endl << std::endl;
 
-    std::string prog_name = argv[0];
+    while (run) {
+        int num_dice = 0;
+        int die_type = 0;
+        int modifier = 0;
+        std::string num_dice_str;
+        std::cout << "Enter in the number of dice to roll: ";
+        if ( !handle_input(num_dice_str, run) ) {
+            continue;
+        }
 
-    printf("Usage: %s: [--help|h] [die] [modifier]\n", prog_name.c_str());
-    printf("Examples:\n");
-    printf("\t%s 1d6\n", prog_name.c_str());
-    printf("\t\tRolls one d6 and prints the result\n");
-    printf("\t%s 2d6 +2\n", prog_name.c_str());
-    printf("\t\tRolls two d6, then adds two and prints the result\n");
-    printf("\t%s d20\n", prog_name.c_str());
-    printf("\t\tRolls one d20 and prints the result\n");
+        std::string die_type_str;
+        std::cout << "Enter in the type of die to roll: ";
+        if ( !handle_input(die_type_str, run) ) {
+            continue;
+        }
+
+        std::string modifier_str;
+        std::cout << "Enter in the modifier: ";
+        if ( !handle_input(modifier_str, run) ) {
+            continue;
+        }
+
+        std::vector<std::string> user_arguments;
+        if ( std::find(die_type_str.begin(), die_type_str.end(), 'd') != die_type_str.end() ) {
+            // User gave us a die type that looks like "d6"
+            user_arguments.push_back( num_dice_str + die_type_str );
+        }
+        else {
+            // User gave us just a number for the die type
+            user_arguments.push_back( num_dice_str + 'd' + die_type_str );
+        }
+        user_arguments.push_back( modifier_str );
+
+        try {
+            parse_options(user_arguments, num_dice, die_type, modifier);
+            roll_dice(num_dice, die_type, modifier);
+            std::cout << std::endl;
+        }
+        catch (Errors e) {
+            switch(e) {
+                case INVALID_NUMBER_DIE:
+                    std::cout << "[" << error_names[e] << "] " << "Invalid number of dice given" << std::endl;
+                    break;
+                case INVALID_DIE_TYPE:
+                    std::cout << "[" << error_names[e] << "] " << "Invalid die type given" << std::endl;
+                    break;
+                case INVALID_MODIFIER:
+                    std::cout << "[" << error_names[e] << "] " << "Invalid modifier given" << std::endl;
+                    break;
+                case UNABLE_CONVERT_NUM_DIE:
+                    std::cout << "[" << error_names[e] << "] " << "Unable to convert the number of dice given into a number" << std::endl;
+                    break;
+                case UNABLE_CONVERT_DIE_TYPE:
+                    std::cout << "[" << error_names[e] << "] " << "Unable to convert the type of die given into a number" << std::endl;
+                    break;
+                case UNABLE_CONVERT_MODIFIER:
+                    std::cout << "[" << error_names[e] << "] " << "Unable to convert the modifier given into a number" << std::endl;
+                    break;
+            }
+        }
+    }
 
     return;
 }
+
+int handle_input (std::string &input, bool &run) {
+    if ( ! (std::cin >> input) ) {
+        std::cout << std::endl << "Goodbye!" << std::endl;
+        run = false;
+
+        return 0;
+    }
+    else if ( input == "q" || input == "quit" ) {
+        std::cout << std::endl << "Goodbye!" << std::endl;
+        run = false;
+
+        return 0;
+    }
+
+    return 1;
+}
+
 
 void roll_dice(int num_dice, int die_type, int modifier) {
     std::random_device rd;
@@ -214,11 +288,29 @@ void roll_dice(int num_dice, int die_type, int modifier) {
     // for ( auto &r : results ) {
     int total = 0;
     for ( int i = 0; i < results.size(); i++ ) {
-        std::cout << "Roll [" << i << "]: " << results[i] << std::endl;
+        std::cout << "Roll [" << i + 1 << "]: " << results[i] << std::endl;
         total += results[i];
     }
     total += modifier;
     std::cout << std::endl << "Total: " << total << std::endl;
+
+    return;
+}
+
+void print_help_text (char *argv[]) {
+
+    std::string prog_name = argv[0];
+
+    printf("Usage: %s: [--help|h] [die] [modifier]\n", prog_name.c_str());
+    printf("Examples:\n");
+    printf("\t%s 1d6\n", prog_name.c_str());
+    printf("\t\tRolls one d6 and prints the result\n");
+    printf("\t%s 2d6 +2\n", prog_name.c_str());
+    printf("\t\tRolls two d6, then adds two and prints the result\n");
+    printf("\t%s d20\n", prog_name.c_str());
+    printf("\t\tRolls one d20 and prints the result\n");
+    printf("\t%s\n", prog_name.c_str());
+    printf("\t\tRuns the program in interactive mode\n");
 
     return;
 }
